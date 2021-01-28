8e35664603697bdba6f5c05dc67926973f9a9d6c
Leonir Kuyava <kuyavinha12@hotmail.com>
2021-01-27 21:41:49 -0300

    Aula dia 27/01/2020

---
+++ models/doacao.js
@@ -0,0 +1,17 @@
+const db = require('./db')
+
+const doacao = db.sequelize.define('doacao',{
+    categoria:{
+        type:db.Sequelize.STRING
+    },
+    descricao:{
+        type:db.Sequelize.STRING
+    },
+    nivel:{
+        type:db.Sequelize.STRING
+    }})
+
+//Cria tabela - somente uma vez
+//doacao.sync({force:true})
+
+module.exports = doacao
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser")
 
 const usuario = require("./models/usuario")
 const ong = require("./models/ong")
+const doacao = require("./models/doacao")
 const { removeData } = require("jquery")
 
 //Configurar handlebar para
@@ -19,6 +20,17 @@ app.set('view engine','handlebars')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+app.use('/static', express.static(__dirname + '/public'));
+
+//chamando ao modeulo express-session 
+var session = require ('express-session');
+
+//configuração da sessão
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.get('/', function (req,res){
     res.render ('paginaInicial')
@@ -29,42 +41,32 @@ app.get('/doeAgora', function(req,res){
     res.render ('doeAgora')
 })
 
-app.get('/login', function(req,res){
-    res.render ('login')
-})
-
 app.get('/cadastro',function(req,res){
     res.render ('cadastro')
 })
-
-app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/cadastroDoacao', function (req,res){
     res.render ('cadastroDoacao')
 
 })
 
-//vamo criar mais uma rota e ela dara para um formulário
-app.get('/update/:id', function(req,res){
-    usuario.findAll({where:{'id' : req.params.id}}).then(function(doadores){
-        res.render('atualiza',{doador: doadores.map(pagamento => pagamento.toJSON())})
-    })
+app.get('/login', function(req,res){
+    res.render ('login')
 })
 
-//depois vamos criar essa rota que envia para o banco e depois chama o formulario
-app.post('/updateUsuario',function(req,res){
-    usuario.update({nome:req.body.nome,senha:req.body.senha},{
-        where:{id:req.body.codigo}}
-    ).then(function(){
-        usuario.findAll().then(function(doadores){
-        res.render('formulario',{doador:doadores.map(pagamento => pagamento.toJSON())})
-    })
-    }).catch(function(erro){
-        res.send("Erro"+erro)
-    })
-})
+app.post('/login',function (req,res){
+//essas duas linhas abaixo vão vir do banco de dados
+    req.session.nome = 'andre';
+    req.session.senha = 'repolho123'
 
-//esse bloco é disparado pelo enviar do formulário
+    if(req.session.nome == req.body.nome && req.body.senha == 'repolho123'){
+        res.send ("usuario logado")
+    }else{
+        res.send ("usuario não existe")
+    }
+})
+//criar cadastro usuario
+
 app.post('/cadUsuario',function(req,res){
     usuario.create({
         nome:req.body.nome,
@@ -94,8 +96,83 @@ app.get('/cadastro',function(req,res){
     })
 })
 
+//vamo criar mais uma rota e ela dara para um formulário
+app.get('/update/:id', function(req,res){
+    usuario.findAll({where:{'id' : req.params.id}}).then(function(doadores){
+        res.render('atualizaUsuario',{doador: doadores.map(pagamento => pagamento.toJSON())})
+    })
+})
 
-//criar tabela ong
+//depois vamos criar essa rota que envia para o banco e depois chama o formulario
+app.post('/updateUsuario',function(req,res){
+    usuario.update({
+            nome:req.body.nome,
+            senha:req.body.senha,
+            email:req.body.email,
+            cpf:req.body.cpf,
+            endereco:req.body.endereco,
+            complemento:req.body.complemento,
+            cidade:req.body.cidade,
+            estado:req.body.estado,
+            cep:req.body.cep,
+            telefoneParaContato:req.body.telefoneParaContato,
+            confirmarDados:req.body.confirmarDados},
+
+            {where:{id:req.body.id}}
+    ).then(function(){
+        usuario.findAll().then(function(doadores){
+        res.render('cadastro',{doador:doadores.map(pagamento => pagamento.toJSON())})
+    })
+    }).catch(function(erro){
+        res.send("Erro"+erro)
+    })
+})
+
+//update ong
+app.get('/update/:id', function(req,res){
+    usuario.findAll({where:{'id' : req.params.id}}).then(function(doadores){
+        res.render('atualizaOng',{doador: doadores.map(pagamento => pagamento.toJSON())})
+    })
+})
+
+//depois vamos criar essa rota que envia para o banco e depois chama o formulario
+app.post('/updateOng',function(req,res){
+    ong.update({nome:req.body.nome,senha:req.body.senha},{
+        where:{id:req.body.codigo}}
+    ).then(function(){
+        ong.findAll().then(function(ongs){
+        res.render('formulario',{ong: ongs.map(cadastramento => cadastramento.toJSON())})
+    })
+    }).catch(function(erro){
+        res.send("Erro"+erro)
+    })
+})
+
+
+//criar cadastro doações
+
+app.post('/cadDoacao',function(req,res){
+    doacao.create({
+        categoria:req.body.categoria,
+        descricao:req.body.descricao,
+        nivel:req.body.nivel
+       
+    }).then(function(){
+        doacao.findAll().then(function(doacoes){
+        res.render('cadastroDoacao', {doacao: doacoes.map(cadastramento => cadastramento.toJSON())})
+    })
+    }).catch(function(erro){
+        res.send("Erro"+erro)
+    })
+
+})
+app.get('/cadDoacao',function(req,res){
+    doacao.findAll().then(function(doacoes){
+        res.render('cadastroDoacao',{doacao:doacoes.map(cadastramento=> cadastramento.toJSON())})
+    })
+})
+
+//criar cadastro ong
 
 app.post('/cadOng',function(req,res){
     ong.create({
@@ -127,13 +204,14 @@ app.get('/cadastroOng',function(req,res){
     })
 })
 
+//Deletar informações
 
 app.get('/delete/:id',function(req,res){
     usuario.destroy({
         where:{'id': req.params.id}
     }).then(function(){
         usuario.findAll().then(function(doadores){
-            res.render('formulario',{doador: doadores.map(
+            res.render('cadastro',{doador: doadores.map(
                 pagamento => pagamento.toJSON())})
         })
 
